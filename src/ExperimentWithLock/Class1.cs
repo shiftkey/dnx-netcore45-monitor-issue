32b4403a3fcfdbeaa4a83017fa041d4308a73344
Brendan Forster <brendan@github.com>
2015-07-27 19:52:12 +0930

    add repro

---
+++ src/ExperimentWithLock/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ExperimentWithLock
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1
+    {
+        object myLock = new object();
+
+        public Class1()
+        {
+            lock(myLock)
+            {
+
+            }
+
+        }
+    }
+}
