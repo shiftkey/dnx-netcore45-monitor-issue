caa4c9982906c612a7db6f7750c3daeaffcf7e66
Brendan Forster <brendan@github.com>
2015-07-28 12:36:22 +0930

    added project which compiles

---
+++ src/CurrentBehaviour/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace CurrentBehaviour
+{
+    public class Class1
+    {
+        object myLock = new object();
+
+        public Class1()
+        {
+            lock (myLock)
+            {
+
+            }
+        }
+    }
+}
